b306843e9199349b22cfee1615e870d7e1bef96f
Abdullah Almalki <Abdul.mo.almalki@gmail.com>
2017-09-25 09:00:36 -0700

    change the info page

--- info.php
+++ info.php
@@ -1,1 +1,7 @@
-this is a basic login page with users names defined in array
+
+<?php
+
+	echo "in this work the index.php file have a basic login system";
+	echo "and it store the user names and passwords in array ";
+	echo "and check if the input match any of the stored usernames and password ";
+?>
