87d20656e0055585646d824518bdce034ad41200
Abdullah Almalki <Abdul.mo.almalki@gmail.com>
2017-09-25 06:33:25 -0700

    changed if statment to loop

--- index.php
+++ index.php
@@ -5,21 +5,18 @@
 	$password = array("12345", "12345", "12345");
 	
 	if(isset($_POST['username']) && isset($_POST['password'])){
-		if($_POST['username'] == $username[0] && $_POST['password'] == $password[0]){
-			$_SESSION['loggedin'] = true;
-			echo "your user name is: ". $username[0];
+		for($i = 0; $i < count($username); $i++){
+			if($_POST['username'] == $username[$i] && $_POST['password'] == $password[$i]){
+				$_SESSION['loggedin'] = true;
+				echo "your user name is: ". $username[$i];
+				break;
+			}
+			else if($_POST['username'] != $username[$i] && $_POST['password'] != $password[$i]){
+				$_SESSION['loggedin'] = false;
+				echo "Warning invaled username or password";
+				break;
+			}	
 		}
-		else if($_POST['username'] == $username[1] && $_POST['password'] == $password[1]){
-			$_SESSION['loggedin'] = true;
-			echo "your user name is: ". $username[1];
-		}
-		else if($_POST['username'] == $username[2] && $_POST['password'] == $password[2]){
-			$_SESSION['loggedin'] = true;
-			echo "your user name is: ". $username[2];
-		}
-		else{
-			echo "Warning invaled username or password";
-		}	
 	}
 ?>
 
