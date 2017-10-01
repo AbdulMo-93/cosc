a4c152d2e307e9079d2b9f016b7abed31efa2c11
Abdullah Almalki <Abdul.mo.almalki@gmail.com>
2017-10-01 10:25:44 -0700

    added the logout button and start session

---
+++ logein.php
@@ -0,0 +1,16 @@
+
+<?php
+	session_start();
+	echo "you're in"
+?>
+
+
+<html>
+<body>
+
+	<form method="post" action="logout.php">	
+		<input type="submit" value="Logout">
+	</form>
+
+</body>
+</html>
