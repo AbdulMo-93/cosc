b7f457c9603749727d2dbbb0169b259cdeba30c5
Abdullah Almalki <Abdul.mo.almalki@gmail.com>
2017-10-01 11:53:27 -0700

    fixed bugs

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE HTML>
 <?php
+	$_SESSION['fail']= 0;
 	session_start();
+
 	$username = array("abdul", "jack", "mo");
 	$password = array("12345", "12345", "12345");
-	$_SESSION['fail'] = 0;
+	
 	
 	if(isset($_POST['username']) && isset($_POST['password'])){
 		for($i = 0; $i < count($username); $i++){
 			if($_POST['username'] == $username[$i] && $_POST['password'] == $password[$i]){
 				$_SESSION['loggedin'] = true;
 				$_SESSION['username'] = $_POST['username'];
-				header("Location: logein.php");
+				$_SESSION['password'] = $_POST['password'];
+				header("Location: welcome.php");
 				break;
 			}
 			else if($_POST['username'] != $username[$i] && $_POST['password'] != $password[$i]){
@@ -30,9 +33,14 @@
 		<input type="text" name="username"><br/>
 		Password<br/>
 		<input type="password" name="password"><br/>		
-		<input type="submit" value="login">
+		<input type="submit" value="Login">
+		
 		
 	</form>
-
+	<form method="post" action= "report.php">
+		<input type="submit" value="Report">
+	</form>
+	
+	
 </body>
 </html>
