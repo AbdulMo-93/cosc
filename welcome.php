1a0bbf174ce756a56d3353afbbec856df49bbe58
Abdullah Almalki <Abdul.mo.almalki@gmail.com>
2017-10-02 04:44:24 -0700

    added refresh redirect authenticated var

--- welcome.php
+++ welcome.php
@@ -3,6 +3,14 @@
 	session_start();
 	echo "Hello " . $_SESSION['username'] . " yor password is " . $_SESSION['password'];
 	echo " Date is " . date("Y/m/d") . "Time is " . date("h/i/s");
+	//for refresh page skip
+	if (!empty($_SESSION['authenticated '])){ 
+		header("Location: logout.php");
+	}
+	else{
+		$_SESSION['authenticated ']=true;
+	}
+	
 ?>
 
 <html>
