65ea636a553d6a5a48c77f1e0ef335cf86e6eb55
Abdullah Almalki <Abdul.mo.almalki@gmail.com>
2017-10-10 06:59:31 -0700

    added a newpage for rejestering an account

---
+++ createAccount.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<?php
+	
+?>
+
+<html>
+<body>
+	Creating Account<br/>
+	  <br/>
+	<form method="post" action="createAccount.php">
+		Username:<br/>
+		<input type="text" name="username"><br/>
+		Password<br/>
+		<input type="password" name="password"><br/>
+		First Name:<br/>
+		<input type="text" name="FirstName"><br/>
+		last Name:<br/>
+		<input type="text" name="LastName"><br/>
+		E-mail:<br/>
+		<input type="text" name="LastName"><br/>
+		  <br/>
+		<input type="submit" value="Login">
+		
+	</form>
+</body>
+</html>
